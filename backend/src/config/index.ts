29c6ba0a070a915825623a6ec38472d978e9e9e4
Antoine A <antoine_avenia@hotmail.fr>
2023-02-08 12:41:08 +0100

    add dotenv config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,4 +1,7 @@
 import merge from "lodash.merge";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
  
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
  
--- backend/src/modules/auth.ts
+++ backend/src/modules/auth.ts
@@ -1,8 +1,8 @@
-import { RequestHandler } from 'express';
-import jwt from 'jsonwebtoken'
-import * as bcrypt from 'bcrypt'
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
 
-import config from "../config" //import env/serv:port
+import config from "../config"; //import env/serv:port
 
 export interface JWTUser {
   id: string;
@@ -10,40 +10,43 @@ export interface JWTUser {
 }
 
 export const createJWT = (user: JWTUser) => {
-  const token = jwt.sign({
-    id: user.id,
-    name: user.username
-  }, (config.secrets.jwt as string))
-  return token
-}
+  const token = jwt.sign(
+    {
+      id: user.id,
+      name: user.username,
+    },
+    config.secrets.jwt as string
+  );
+  return token;
+};
 
 export const protect: RequestHandler = (req, res, next) => {
-  const bearer = req.headers.authorization
+  const bearer = req.headers.authorization;
 
   if (!bearer) {
-    return res.status(401).json({ message: 'Unauthorized' })
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const [, token] = bearer.split(' ')
+  const [, token] = bearer.split(" ");
 
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' })
+    return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
-    const payload = jwt.verify(token, (config.secrets.jwt as string)) as JWTUser
-    req.user = payload
-    return next()
-  } catch(e) {
-    console.error(e)
-    return res.status(401).json({ message: 'Unauthorized' })
+    const payload = jwt.verify(token, config.secrets.jwt as string) as JWTUser;
+    req.user = payload;
+    return next();
+  } catch (e) {
+    console.error(e);
+    return res.status(401).json({ message: "Unauthorized" });
   }
-}
+};
 
 export const hashPassword = (password: string) => {
-  return bcrypt.hash(password, 10)
-}
+  return bcrypt.hash(password, 10);
+};
 
 export const comparePassword = (password: string, hash: string) => {
-  return bcrypt.compare(password, hash)
-}
+  return bcrypt.compare(password, hash);
+};
