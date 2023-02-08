7092220f10c4c5afb1fd3f6ef80c7ae5431ef1dc
PICOUT Tom <79899852+Tommm77@users.noreply.github.com>
2023-02-08 12:17:24 +0100

    📦 NEW: Add cors to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import userRouter from './routes/user'
 import todoListRouter from './routes/todoList'
 import todoItemRouter from './routes/todoItem'
 import { createNewUser, signIn } from './handlers/user'
+import cors from 'cors'
 
 import config from "./config" //import env/serv:port
 
@@ -13,6 +14,8 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.get('/', async (req, res) => {
   return res.json({ message: 'Bonsoir' })
